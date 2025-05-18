4fe1fc36efb32d0de03847d9942ea6f0c1af6a05
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-05-18 08:42:37 +0530

    Logic for tic tac game being started

---
+++ app.js
@@ -0,0 +1,29 @@
+const boxes = document.querySelectorAll(".box");
+const reset_game = document.querySelector(".reset_game");
+
+let turnO = true;
+
+const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+]
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () =>{
+        if(turnO){
+            box.innerText = "O";
+            turnO = false;
+        }
+        else{
+            box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true;
+    });
+});
